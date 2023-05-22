d2af2d98608cc0510c948633ce929b1ef3913f08
José Jorge Figueroa Figueroa <figuejojo6@gmail.com>
2023-05-22 13:26:33 +0100

    FEAT(RTOS): FreeRTOS was included to the project and BLINK example was tested.

--- main.c
+++ main.c
@@ -1,13 +1,27 @@
+#include <FreeRTOS.h>
+#include <task.h>
+#include <stdio.h>
 #include "pico/stdlib.h"
 
-int main() {
+void led_task()
+{   
     const uint LED_PIN = 3;
     gpio_init(LED_PIN);
     gpio_set_dir(LED_PIN, GPIO_OUT);
     while (true) {
         gpio_put(LED_PIN, 1);
-        sleep_ms(250);
+        vTaskDelay(100); // Ticks to delay
         gpio_put(LED_PIN, 0);
-        sleep_ms(250);
+        vTaskDelay(100);
     }
+}
+
+int main()
+{
+    stdio_init_all();
+
+    xTaskCreate(led_task, "LED_Task", 256, NULL, 1, NULL);
+    vTaskStartScheduler();
+
+    while(1){};
 }
