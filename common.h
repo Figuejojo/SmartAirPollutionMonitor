1f1d07bc4b0601be0d4f46d50bf186e691ba5673
José Jorge Figueroa Figueroa <figuejojo6@gmail.com>
2023-06-10 16:33:41 +0100

    FEAT(DEBUG): USB Debug print was setup to help us develop more easily.

---
+++ common.h
@@ -0,0 +1,24 @@
+/** @file Common.h
+ *  @brief Header file intended to be call in all documents. It will contian elements that will be used across
+ *          most of the files.
+ *
+ *  @author 
+ */
+
+#ifndef _COMMON_H_
+#define _COMMON_H_
+
+/*******************************************************************************
+* Includes
+*******************************************************************************/
+#include <FreeRTOS.h>
+#include <FreeRTOS-Kernel/include/queue.h>
+#include <task.h>
+#include <stdio.h>
+#include "pico/stdlib.h"
+
+/*******************************************************************************
+* Type definitions
+*******************************************************************************/
+
+#endif //_COMMON_H_
--- main.c
+++ main.c
@@ -1,24 +1,27 @@
-#include <FreeRTOS.h>
-#include <task.h>
-#include <stdio.h>
-#include "pico/stdlib.h"
+#include "common.h"
+
+QueueHandle_t xQueue1;
 
 void led_task()
 {   
+
     const uint LED_PIN = 3;
     gpio_init(LED_PIN);
     gpio_set_dir(LED_PIN, GPIO_OUT);
     while (true) {
         gpio_put(LED_PIN, 1);
-        vTaskDelay(100); // Ticks to delay
+        vTaskDelay(500/portTICK_PERIOD_MS); // Ticks to delay
         gpio_put(LED_PIN, 0);
-        vTaskDelay(100);
+        vTaskDelay(500/portTICK_PERIOD_MS);
+        printf("Blinking!\r\n");
     }
 }
 
 int main()
 {
     stdio_init_all();
+
+    xQueue1 = xQueueCreate(10, sizeof(int));
 
     xTaskCreate(led_task, "LED_Task", 256, NULL, 1, NULL);
     vTaskStartScheduler();
