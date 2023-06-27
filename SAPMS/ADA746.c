4c0ebd002bc4f7fa3708cf77093070031b8da181
LAPTOP-R0TE2KN3\SH <shshenhao806gmail.com>
2023-06-27 13:56:19 +0100

    feat(GPS): Basic structure.

---
+++ SAPMS/ADA746.c
@@ -0,0 +1,35 @@
+/** @file ADA746.c
+ *  @brief  //Write a brief description 
+ *
+ *  @author //Exam Id
+ */
+#include "ADA746.h"
+
+/*******************************************************************************
+* Static & Global Variables
+*******************************************************************************/
+
+/*******************************************************************************
+* Function Definition
+*******************************************************************************/
+
+/**
+*	@name vTaskGPS
+*   @type Task
+*/
+void vTaskGPS(void * pvParameters)
+{
+    while(1)
+    {
+
+    }
+}
+
+/**
+*	@name  GPS_setup
+*   @Type  Setup functon
+*/
+void GPS_setup(void)
+{
+
+}
