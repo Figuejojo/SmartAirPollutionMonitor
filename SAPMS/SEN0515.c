a61ed1d50889ec9ff46d07f427259de1dc4c4da4
sj1453 <sj1453@york.ac.uk>
2023-07-18 12:33:34 +0100

    feat(ENS160): add the configuration for the I2C1

--- SAPMS/SEN0515.c
+++ SAPMS/SEN0515.c
@@ -1,17 +1,18 @@
-/** @file ADA746.c
- *  @brief  //Write a brief description 
+/** @file sen0515.c
+ *  @brief  // Setting up the I2C1 peripheral in the Raspberry Pi Pico to be used for the SEN0515/ENS160
  *
  *  @author //Exam Id
  */
 #include "SEN0515.h"
 
-/*******************************************************************************
-* Static & Global Variables
-*******************************************************************************/
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "hardware/i2c.h"
 
-/*******************************************************************************
-* Function Definition
-*******************************************************************************/
+#define I2C_PORT i2c1
+#define I2C_SDA_PIN 18
+#define I2C_SCL_PIN 19
+#define I2C_FREQUENCY 100000 // I2C bus frequency in Hz
 
 /**
 *	@name vTaskGPS
@@ -22,8 +23,24 @@ void vTaskSEN0515(void * pvParameters)
     //uint8_t msg[] = "TVOC";
     while(1)
     {
+#if 0 
+        // Example: Read data from SEN0515/ENS160
+        uint8_t reg_address = 0x00; // Change this to the desired register address
+
+        // Start the I2C transaction
+        i2c_write_blocking(I2C_PORT, device_address, &reg_address, 1, true);
+
+        // Read data from SEN0515/ENS160
+        uint8_t data[2];
+        i2c_read_blocking(I2C_PORT, device_address, data, sizeof(data), false);
+
+        // Process the data (e.g., print it)
+        printf("Data: 0x%02X%02X\n", data[0], data[1]);
+
+        
         vTaskDelay(1000/portTICK_PERIOD_MS);
         //Print_debug(msg);
+#endif
     }
 }
 
@@ -34,6 +51,11 @@ void vTaskSEN0515(void * pvParameters)
 void setupSEN0515(void)
 {
 #if USE_SEN0515 == 1
-//Implementation here
-#endif
+    i2c_init(I2C_PORT, I2C_FREQUENCY);
+    gpio_set_function(I2C_SDA_PIN, GPIO_FUNC_I2C);
+    gpio_set_function(I2C_SCL_PIN, GPIO_FUNC_I2C);
+    gpio_pull_up(I2C_SCL_PIN);
+    gpio_pull_up(I2C_SCL_PIN);  
+
+  #endif
 }
