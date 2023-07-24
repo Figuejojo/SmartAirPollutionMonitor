447f8f84dc41289be82f1748cc089716b47aee81
José Jorge Figueroa Figueroa <figuejojo6@gmail.com>
2023-07-24 17:09:56 +0100

    feat(UDP):implement a task for sending UDP messages for beacon.

--- WIFI/wifi_mqtt.c
+++ WIFI/wifi_mqtt.c
@@ -9,12 +9,16 @@
 *******************************************************************************/
 #include "wifi_mqtt.h"
 #include "pico/cyw43_arch.h"
-
+#include "lwip/pbuf.h"
+#include "lwip/udp.h"
 
 /*******************************************************************************
 * Static Global Variables
 *******************************************************************************/
-
+#define UDP_PORT 4444
+#define BEACON_MSG_LEN_MAX 127
+#define BEACON_TARGET "255.255.255.255"
+#define BEACON_INTERVAL_MS 1000
 
 /*******************************************************************************
 * Static Function Declarations
@@ -25,21 +29,49 @@
 */
 void vTaskWireless(void * pvParameters)
 {
-    if (cyw43_arch_init()) {
+    if (cyw43_arch_init()) 
+    {
+        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, false);
         printf("failed to initialise\n");
     }
     cyw43_arch_enable_sta_mode();
     printf("Connecting to Wi-Fi...\n");
-    if (cyw43_arch_wifi_connect_timeout_ms(SSID_WIFI, PSWD_WIFI, CYW43_AUTH_WPA2_AES_PSK, 30000)) {
+    if (cyw43_arch_wifi_connect_timeout_ms(SSID_WIFI, PSWD_WIFI, CYW43_AUTH_WPA2_AES_PSK, 30000)) 
+    {
+        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, false);
         printf("failed to connect.\n");
-    } else {
+    } 
+    else 
+    {
         printf("Connected.\n");
+        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, true);
     }
-
+    struct udp_pcb* pcb = udp_new();
+    ip_addr_t addr;
+    ipaddr_aton(BEACON_TARGET, &addr);
+    int counter = 0;
     while(1)
     {
-        
-        vTaskDelay(300/portTICK_PERIOD_MS);
+        if(0 > cyw43_wifi_link_status(&cyw43_state, CYW43_ITF_STA))
+        {
+           printf("Disconnected\n"); 
+        }
+        else
+        {
+            struct pbuf *p = pbuf_alloc(PBUF_TRANSPORT, BEACON_MSG_LEN_MAX+1, PBUF_RAM);
+            char *req = (char *)p->payload;
+            memset(req, 0, BEACON_MSG_LEN_MAX+1);
+            snprintf(req, BEACON_MSG_LEN_MAX, "%d\n", counter);
+            err_t er = udp_sendto(pcb, p, &addr, UDP_PORT);
+            pbuf_free(p);
+            if (er != ERR_OK) {
+                printf("Failed to send UDP packet! error=%d", er);
+            } else {
+                printf("Sent packet %d\n", counter);
+                counter++;
+            }
+        }
+        vTaskDelay(5000/portTICK_PERIOD_MS);
     }
 }
 
