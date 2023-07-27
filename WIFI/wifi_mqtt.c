75d1e500264c68fc20cfbba7ddb6d49cdbd3c8a4
José Jorge Figueroa Figueroa <figuejojo6@gmail.com>
2023-07-27 18:28:48 +0100

    chore(CywAz): clean proyect

--- WIFI/wifi_mqtt.c
+++ WIFI/wifi_mqtt.c
@@ -9,16 +9,14 @@
 *******************************************************************************/
 #include "wifi_mqtt.h"
 #include "pico/cyw43_arch.h"
-#include "lwip/pbuf.h"
-#include "lwip/udp.h"
+
+#include "lwip/dns.h"
+#include "lwip/apps/mqtt.h"
 
 /*******************************************************************************
 * Static Global Variables
 *******************************************************************************/
-#define UDP_PORT 4444
-#define BEACON_MSG_LEN_MAX 127
-#define BEACON_TARGET "255.255.255.255"
-#define BEACON_INTERVAL_MS 1000
+
 
 /*******************************************************************************
 * Static Function Declarations
@@ -32,12 +30,17 @@ static void svConnect();
 */
 void vTaskWireless(void * pvParameters)
 {
+    struct netif fsl_netif0;
+    ip4_addr_t fsl_netif0_ipaddr, fsl_netif0_netmask, fsl_netif0_gw;
+
+    if (cyw43_arch_init()) 
+    {
+        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, false);
+        printf("failed to initialise\n");
+    }
+    cyw43_arch_enable_sta_mode();
     svConnect();
 
-    struct udp_pcb* pcb = udp_new();
-    ip_addr_t addr;
-    ipaddr_aton(BEACON_TARGET, &addr);
-    int counter = 0;
     while(1)
     {
         if(0 > cyw43_wifi_link_status(&cyw43_state, CYW43_ITF_STA))
@@ -47,19 +50,7 @@ void vTaskWireless(void * pvParameters)
         }
         else
         {
-            struct pbuf *p = pbuf_alloc(PBUF_TRANSPORT, BEACON_MSG_LEN_MAX+1, PBUF_RAM);
-            char *req = (char *)p->payload;
-            memset(req, 0, BEACON_MSG_LEN_MAX+1);
-            snprintf(req, BEACON_MSG_LEN_MAX, "%d\n", counter);
-            err_t er = udp_sendto(pcb, p, &addr, UDP_PORT);
-            pbuf_free(p);
-            if (er != ERR_OK) {
-                printf("Failed to send UDP packet! error=%d", er);
-                cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, false);
-            } else {
-                printf("Sent packet %d\n", counter);
-                counter++;
-            }
+            
         }
         vTaskDelay(5000/portTICK_PERIOD_MS);
     }
@@ -76,13 +67,6 @@ ERR_t vSetupWifi(void)
 
 void svConnect()
 {
-    if (cyw43_arch_init()) 
-    {
-        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, false);
-        printf("failed to initialise\n");
-    }
-    cyw43_arch_enable_sta_mode();
-
     printf("Connecting to Wi-Fi...\n");
     if (cyw43_arch_wifi_connect_timeout_ms(SSID_WIFI, PSWD_WIFI, CYW43_AUTH_WPA2_AES_PSK, 30000)) 
     {
@@ -95,3 +79,5 @@ void svConnect()
         cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, true);
     }
 }
+
+
