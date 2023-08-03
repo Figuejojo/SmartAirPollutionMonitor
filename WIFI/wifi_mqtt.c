87ecae666205bcf9164a91c82d842015998c49f6
José Jorge Figueroa Figueroa <figuejojo6@gmail.com>
2023-08-03 11:59:09 +0100

    feat(HTTP): send one value to Thinkspeak using http.

--- WIFI/wifi_mqtt.c
+++ WIFI/wifi_mqtt.c
@@ -11,12 +11,13 @@
 #include "pico/cyw43_arch.h"
 #include "lwip/pbuf.h"
 #include "lwip/udp.h"
+#include "lwip/tcp.h"
 #include "lwip/apps/httpd.h"
 #include "lwip/dns.h"
 /*******************************************************************************
 * Static Global Variables
 *******************************************************************************/
-#define UDP_PORT 5005 //4444
+#define UDP_PORT 80//5005 //4444
 #define BEACON_MSG_LEN_MAX 127
 #define BEACON_TARGET "255.255.255.255"
 #define BEACON_INTERVAL_MS 1000
@@ -25,9 +26,15 @@
 * Static Function Declarations
 *******************************************************************************/
 static void svConnect();
+static void tcp_setup(void);
 static void dns_dorequest(void *arg);
+static uint32_t tcp_send_packet(void);
+static err_t tcpRecvCallback(void *arg, struct tcp_pcb *tpcb, struct pbuf *p, err_t err);
+static err_t connectCallback(void *arg, struct tcp_pcb *tpcb, err_t err);
+
 ip_addr_t dnsRespIP;
 ip_addr_t IPBackup;
+struct tcp_pcb *testpcb;
 /*******************************************************************************
 * Static global variables
 *******************************************************************************/
@@ -39,13 +46,14 @@ ip_addr_t IPBackup;
 void vTaskWireless(void * pvParameters)
 {
     svConnect();
-
-
+    vTaskDelay(500);
+    dns_dorequest(NULL);
+    vTaskDelay(500);
+    tcp_setup();
+    vTaskDelay(500);
     
-    struct tcp_pcb * tcp_new(void);
-    //struct udp_pcb* pcb = udp_new();
-
     int counter = 0;
+    volatile err_t err = 0;
     while(1)
     {
         if(0 > cyw43_wifi_link_status(&cyw43_state, CYW43_ITF_STA))
@@ -55,10 +63,12 @@ void vTaskWireless(void * pvParameters)
         }
         else
         {
-            dns_dorequest(NULL);
-            printf("WIFIStuff\n");
+            Print_debug("Openning->");
+            ip_addr_t ip = IPBackup;
+            tcp_connect(testpcb, &ip, 80, connectCallback);
+
         }
-        vTaskDelay(5000/portTICK_PERIOD_MS);
+        vTaskDelay(60000*5/portTICK_PERIOD_MS);
     }
 }
 
@@ -112,38 +122,78 @@ void dns_dorequest(void *arg)
   }
 }
 
-void httpd_post_finished(void *connection, char *response_uri, u16_t response_uri_len)
+err_t connectCallback(void *arg, struct tcp_pcb *tpcb, err_t err)
 {
-    
-}
-
-err_t httpd_post_receive_data(void *connection, struct pbuf *p)
-{
+    printf("Connection Established -> ");
+    tcp_send_packet();
     return 0;
 }
 
-err_t httpd_post_begin(void *connection, const char *uri, const char *http_request,
-                       u16_t http_request_len, int content_len, char *response_uri,
-                       u16_t response_uri_len, u8_t *post_auto_wnd)
+uint32_t tcp_send_packet(void)
 {
-    return 0;    
+    printf("Now sending a packet\n");
+    err_t error;
+
+    static uint32_t sensorData = 40;
+    char string[130] = {0};
+    sprintf(string, "GET /update?api_key=VD7UI0ZJ7DPOEHGC&field1=%d HTTP/1.0\r\nConnection: Close\r\nHost: api.thingspeak.com\r\n\r\n ",sensorData++);
+
+    /* push to buffer */
+    error = tcp_write(testpcb, string, strlen(string), TCP_WRITE_FLAG_COPY);
+
+    if (error) {
+        printf("ERROR: Code: %d (tcp_send_packet :: tcp_write)\n", error);
+        return 1;
+    }
+
+    /* now send */
+    error = tcp_output(testpcb);
+    if (error) {
+        printf("ERROR: Code: %d (tcp_send_packet :: tcp_output)\n", error);
+        return 1;
+    }
+    return 0;
 }
 
-void vSendUdp()
+err_t tcpRecvCallback(void *arg, struct tcp_pcb *tpcb, struct pbuf *p, err_t err)
 {
-    #if 0 
-    struct pbuf *p = pbuf_alloc(PBUF_TRANSPORT, BEACON_MSG_LEN_MAX+1, PBUF_RAM);
-    char *req = (char *)p->payload;
-    memset(req, 0, BEACON_MSG_LEN_MAX+1);
-    snprintf(req, BEACON_MSG_LEN_MAX, "%d\n", counter);
-    err_t er = udp_sendto(pcb, p, &addr, UDP_PORT);
-    pbuf_free(p);
-    if (er != ERR_OK) {
-        printf("Failed to send UDP packet! error=%d", er);
-        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, false);
+    printf("Data recieved.\n");
+    if (p == NULL) {
+        printf("The remote host closed the connection.\n");
+        printf("Now I'm closing the connection.\n");
+        tcp_close(testpcb);
+        return ERR_ABRT;
     } else {
-        printf("Sent packet %d\n", counter);
-        counter++;
+        printf("Number of pbufs %d\n", pbuf_clen(p));
+        printf("Contents of pbuf %s\n", (char *)p->payload);
     }
-    #endif
+    Print_debug("Closing tcp");
+    if(ERR_OK == tcp_close(testpcb))
+    {
+        Print_debug("Closed tcp");
+    }
+    return 0;
+}
+
+void tcp_setup(void)
+{
+    uint32_t data = 0xdeadbeef;
+
+    /* create an ip */
+    ip_addr_t ip = IPBackup; //Change for thinkspeak??//Thinkspeak 54.81.196.77
+
+    /* create the control block */
+    testpcb = tcp_new();    
+
+    /* dummy data to pass to callbacks*/
+    tcp_arg(testpcb, &data);
+
+    /* register callbacks with the pcb */
+    tcp_err(testpcb, NULL);
+    tcp_recv(testpcb, tcpRecvCallback);
+    tcp_sent(testpcb, NULL);
+
+    /* now connect */
+    //tcp_connect(testpcb, &ip, 80, connectCallback);
+    
 }
