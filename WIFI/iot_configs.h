121bf77c20199d685b7f9d5223abef2a07f85641
José Jorge Figueroa Figueroa <figuejojo6@gmail.com>
2023-08-19 21:20:14 +0100

    Test(IOTCONG): add place holder element for testing

--- WIFI/iot_configs.h
+++ WIFI/iot_configs.h
@@ -23,23 +23,25 @@
     #define HTTP_CMD "/update?"
     #define HTTP_KEY "<Key>"    //Write the API Key.
     #define HTTP_FIELD "&field1="
-#endif 
-
-#if(AZ_TS == 1)
+#elif(AZ_TS == 1)
     #define IOT_CONFIG_IOTHUB_FQDN  "<HUB-Name>"        //!< IoTHub Name 
     #define IOT_CONFIG_DPS_ID_SCOPE "<Scope-ID>"   //!< Scope ID
     #define IOT_CONFIG_DEVICE_ID    "<DEV-ID>"         //APMIOT_002" //!< Device ID
     #define IOT_CONFIG_DEVICE_KEY   "<PRIM-KEY>"      //"DRtgC6kb0mNrs+SICxHbJ9wPTMnAE37gG/stbDnByzY="        
     #define ENDPOINT ""
-#endif
-
-#if(MQTT_TS == 1)
+#elif(MQTT_TS == 1)
     #define ENDPOINT   "mqtt3.thingspeak.com" 
     #define IOT_CLIENT "<Client ID>"
     #define IOT_USER   "<User ID(Same as Client ID)>"
     #define IOT_PWD    "<Password>"
     #define IOT_TOPIC  "<Topic/Subtopic>"
-#endif 
+#else
+    #define IOT_TOPIC  "<Topic/PlaceHold>"
+    #define IOT_CLIENT "<PlaceHolder>"
+    #define IOT_USER   "<PlaceHolder>"
+    #define IOT_PWD    "<PlaceHolder>"  
+    #define ENDPOINT "<PlaceHolder>"
+#endif
 
 // Publish 1 message every 2 seconds
 #define TELEMETRY_FREQUENCY_MILLISECS 2000
